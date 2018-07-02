b7199b2d3e5ace2da2a84cc5fd69c09a946c4dd3
Derek <derek.chan522@gmail.com>
2018-07-02 14:45:10 +0800

    add CRUD functions

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 3000;
+
+let pizzas = [
+  { id: "1", name: "pepperoni pizza", price: 20 },
+  { id: "2", name: "hawaiian pizza", price: 16 }
+];
+
+app.use(bodyParser());
+
+app.get("/pizza", (req, res) => {
+  res.send(pizzas);
+});
+
+app.get("/pizza/:id", (req, res) => {
+  res.send(pizzas.find(element => element.id == req.params.id));
+});
+
+app.post("/pizza", (req, res) => {
+  pizzas = [...pizzas, req.body];
+  res.send(pizzas);
+});
+
+app.put("/pizza/:id", (req, res) => {
+  pizzas = pizzas.map(pizza => {
+    if (pizza.id == req.params.id) return Object.assign(pizza, req.body);
+    else return pizza;
+  });
+
+  res.send(pizzas);
+});
+
+app.delete("/pizza/:id", (req, res) => {
+  pizzas = pizzas.filter(pizza => pizza.id != req.params.id);
+  res.send(pizzas);
+});
+
+app.listen(PORT, () => {
+  console.log(`awesome express pizza app has started on PORT ${PORT}`);
+});
